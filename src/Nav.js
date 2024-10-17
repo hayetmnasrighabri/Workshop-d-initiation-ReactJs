338b3bd59f5df5da7b0c5eae8c918fccbd7ce75e
hayetmnasri <hayetmnasri@hotmail.fr>
2024-10-17 15:58:59 +0200

    Links

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Nav(props) {
   return (
     <div>
@@ -12,10 +12,10 @@ function Nav(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/Home">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <Link className="nav-link" to="/contact">Contact</Link>
         </li>
           </ul>     
     </div>
